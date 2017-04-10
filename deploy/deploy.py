35b969b69017d0d2109d7c5a0a1e0b315e5ee487
Lode Lesage <musilitar@gmail.com>
2017-04-10 22:01:36 +0200

    Update deploy.py

--- deploy/deploy.py
+++ deploy/deploy.py
@@ -6,6 +6,6 @@ from subprocess import check_call
 @hug.call("/deploy", ["POST"], versions=1)
 def docs():
     """Trigger a deployment of the application"""
-    check_call(["git", "pull", "/home/Musilitar/MusilitarAPI"])
+    check_call(["git", "--work-tree=/home/Musilitar/MusilitarAPI", "--git-dir=/home/Musilitar/MusilitarAPI/.git", "pull"])
     check_call(["touch", "/var/www/www_musilitar_org_wsgi.py"])
     return {"status": "deployed"}
