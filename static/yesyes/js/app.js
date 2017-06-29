30653401bb4f5e39a9b39600cc8ac8a794b423b7
musilitar <musilitar@gmail.com>
2017-06-29 22:17:20 +0200

    added dropdown selection of board size

--- static/yesyes/js/app.js
+++ static/yesyes/js/app.js
@@ -1,11 +1,47 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", function()
 {
-    let board = new Vue
+    Vue.component("dropdown", 
+    {
+        template: "#dropdown",
+        props: ["model", "options"],
+        data: function()
+        {
+            let data =
+            {
+                open: false
+            };
+
+            return data;
+        },
+        methods: 
+        {
+            selectOption: function(option)
+            {
+                this.$emit("option-selected", option);
+            },
+            toggleOpen: function()
+            {
+                this.open = !this.open;
+            }
+        }
+    });
+    
+    let app = new Vue
     ({
-        el: "#board",
+        el: "#app",
         data: 
         {
+            availableSizes: [8, 10, 12, 14, 16],
             boardSize: 10
+        },
+        methods:
+        {
+            onSizeSelected: function(size)
+            {
+                this.boardSize = size;
+            }
         }
     });
 });
