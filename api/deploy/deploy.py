ea2fdb59711bf68438f4f4c1e0861a859538992d
musilitar <musilitar@gmail.com>
2017-10-23 20:37:25 +0200

    fixed syntax error

--- api/deploy/deploy.py
+++ api/deploy/deploy.py
@@ -7,10 +7,6 @@ from subprocess import check_call
 def docs():
     """Trigger a deployment of the application"""
     check_call(["git", "pull"], cwd="/home/Musilitar/MusilitarAPI")
-    check_call
-    (
-        ["pipenv", "install", "--skip-lock"],
-        cwd="/home/Musilitar/MusilitarAPI"
-    )
+    check_call(["pipenv", "install", "--skip-lock"], cwd="/home/Musilitar/MusilitarAPI")
     check_call(["touch", "/var/www/www_musilitar_org_wsgi.py"])
     return {"status": "deployed"}
