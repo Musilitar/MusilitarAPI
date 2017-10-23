2b267449102f1bfe9cc17ee1682e9a484087798d
musilitar <musilitar@gmail.com>
2017-10-23 20:35:26 +0200

    removed Pipfile.lock because of constant errors, updated Pipfile to use specific versions, updated deploy scripts to change working directory

--- api/deploy/deploy.py
+++ api/deploy/deploy.py
@@ -6,7 +6,11 @@ from subprocess import check_call
 @hug.call("/deploy", ["POST"], versions=1)
 def docs():
     """Trigger a deployment of the application"""
-    check_call(["git", "--work-tree=/home/Musilitar/MusilitarAPI", "--git-dir=/home/Musilitar/MusilitarAPI/.git", "pull"])
-    check_call(["pipenv", "install"])
+    check_call(["git", "pull"], cwd="/home/Musilitar/MusilitarAPI")
+    check_call
+    (
+        ["pipenv", "install", "--skip-lock"],
+        cwd="/home/Musilitar/MusilitarAPI"
+    )
     check_call(["touch", "/var/www/www_musilitar_org_wsgi.py"])
     return {"status": "deployed"}
